76b51d057a8456a759907f458a32400d6c8628dc
Daniel Greenidge <dev@danielgreenidge.com>
2020-03-16 13:41:58 -0400

    Switch dataset to integer count features

--- tests/test_pid.py
+++ tests/test_pid.py
@@ -3,13 +3,25 @@ from typing import Tuple
 
 import numpy as np
 import pytest
+from sklearn import model_selection
 
 from gdec import pid, synthetic
 
 
 @pytest.fixture(scope="module")
 def dataset() -> Tuple[np.ndarray, np.ndarray]:
-    return synthetic.generate_dataset(seed=1434, n_classes=32)
+    return synthetic.generate_dataset(seed=1634, n_classes=32)
+
+
+def test_you_can_train_pid_on_the_synthetic_dataset():
+    X, y = synthetic.generate_dataset(seed=1634, n_classes=32)
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(
+        X, y, train_size=0.8
+    )
+    model = pid.PID()
+    model.fit(X_train, y_train)
+    score = model.score(X_test, y_test)
+    assert score > 1 / 32  # Better than random guessing?
 
 
 def test_training_pid_on_float_dataset_raises_value_error(dataset):
--- tests/test_synthetic.py
+++ tests/test_synthetic.py
@@ -5,11 +5,11 @@ from gdec import synthetic
 
 
 def test_you_can_train_logistic_regression_on_the_synthetic_dataset():
-    X, y = synthetic.generate_dataset(seed=1434, n_classes=32)
+    X, y = synthetic.generate_dataset(seed=1634, n_classes=32)
     X_train, X_test, y_train, y_test = model_selection.train_test_split(
         X, y, train_size=0.8
     )
-    model = linear_model.LogisticRegression()
+    model = linear_model.LogisticRegression(solver="newton-cg")
     model.fit(X_train, y_train)
     score = model.score(X_test, y_test)
     assert score > 1 / 32  # Better than random guessing?
