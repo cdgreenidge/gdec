cab6f8f7b309db9353999390c92daf77a8ac63df
Daniel Greenidge <dev@danielgreenidge.com>
2020-03-16 16:03:09 -0400

    Switch to generative synthetic data

--- gdec/synthetic.py
+++ gdec/synthetic.py
@@ -1,28 +1,32 @@
 """Synthetic dataset generation."""
+import math
 from typing import Tuple
 
 import numpy as np
-from scipy import special, stats
-
-from gdec import npgp, useful
+from scipy import stats
 
 
 def generate_dataset(
-    amplitude=1.0, lengthscale=0.1, n_classes=32, n_data=1024, n_features=32, seed=1614
+    curve_max: float = 2,
+    examples_per_class: int = 32,
+    kappa: float = 5,
+    n_classes: int = 32,
+    n_features: int = 16,
+    seed: int = 42,
 ) -> Tuple[np.ndarray, np.ndarray]:
     """Generate a synthetic dataset for testing decoders.
 
+    Each neuron has a Von Mises bump tuning curve, and they are randomly correlated
+    together.
+
     Args:
-        amplitude: The Fourier-domain GP prior amplitude for the weight matrix, in
-            [0, infinity).
-        lengthscale: The Fourier domain GP prior lengthscale for the weight matrix, in
-            [0, 1].
+        curve_max: The maximum value of the tuning curve (firing rate) for each neuron.
+        examples_per_class: The number of examples per class.
+        kappa: The shape parameter of the Von Mises bump tuning curves. Higher is
+            sharper.
         n_classes: The number of classes in the dataset.
-        n_data: The number of examples in the datset.
-        n_features: The number of features/neurons in the dataset.
-        seed: The random seed. The default, is chosen to generate a dataset with
-            a low discrepancy between the number of examples in the least common class
-            and the number of examples in the most common class.
+        n_features: The number of features (neurons) in the dataset.
+        seed: The random seed.
 
     Returns:
         A tuple (X, y) of examples, of shape (n_data, n_features) and labels, of shape
@@ -31,25 +35,23 @@ def generate_dataset(
 
     """
     np.random.seed(seed)
-    f_mean = np.zeros((n_features,))
-    f_cov = stats.invwishart(df=n_features, scale=0.1 * np.eye(n_features)).rvs()
-    F = stats.multivariate_normal(mean=f_mean, cov=f_cov).rvs(n_data)
+    classes = np.arange(n_classes)
 
-    def link(x: np.ndarray) -> np.ndarray:
-        """Link function for likelihood."""
-        return 5 * special.expit(x) + 1
+    classes_rad = (classes / n_classes) * 2 * math.pi
+    feature_tuning_locs = np.linspace(classes_rad[0], classes_rad[-1], num=n_features)
+    tuning_curves = np.empty((n_classes, n_features))
+    for i, loc in enumerate(feature_tuning_locs):
+        curve = stats.vonmises.pdf(classes_rad, kappa=5, loc=loc)
+        curve = curve_max * (curve / np.max(curve))
+        tuning_curves[:, i] = curve
 
-    X = stats.poisson.rvs(link(F))
+    y = classes.repeat(examples_per_class)
 
-    def spectrum(w: np.ndarray) -> np.ndarray:
-        """GP power spectrum, evaluated elementwise."""
-        return npgp.rbf_spectrum(w, amplitude, lengthscale)
+    # Sampling strategy: Use Sklar's theorem to sample correlated Poisson RV using
+    # copula + Poisson marginals. We generate the copula using a multivariate normal.
+    sigma = stats.invwishart.rvs(df=n_features, scale=np.eye(n_features))
+    normal_samples = stats.multivariate_normal.rvs(cov=sigma, size=y.size)
+    copula = stats.norm.cdf(normal_samples, scale=np.sqrt(np.diagonal(sigma)))
+    X = stats.poisson.ppf(copula, mu=tuning_curves[y, :]).astype(int)
 
-    n_funs = npgp.choose_n_basis_funs(spectrum)
-    basis = npgp.whitened_fourier_basis(spectrum, n_domain=n_classes, n_funs=n_funs)
-    spectral_coefs = np.random.randn(n_funs, n_features)
-    W = basis @ spectral_coefs
-
-    probs = np.exp(useful.log_softmax(X @ W.T, axis=-1))
-    classes = useful.categorical_sample(probs)
-    return X, classes
+    return X, y
