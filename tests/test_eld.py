4342597e253572a49a829b593bce9f580edb5a12
Daniel Greenidge <dev@danielgreenidge.com>
2020-03-17 17:27:08 -0400

    Switch dataset generation to fixture

--- tests/test_eld.py
+++ tests/test_eld.py
@@ -1,9 +1,19 @@
 """Test eld.py."""
+from typing import Tuple
+
 import numpy as np
+import pytest
 from sklearn import model_selection
 
 import gdec
 from gdec import eld, synthetic
+
+
+@pytest.fixture(scope="module")
+def dataset() -> Tuple[np.ndarray, np.ndarray]:
+    return synthetic.generate_dataset(
+        seed=1634, examples_per_class=8, n_classes=8, n_features=8
+    )
 
 
 def test_circdist_gives_correct_distances():
@@ -15,23 +25,23 @@ def test_circdist_gives_correct_distances():
     assert np.array_equal(actual, expected)
 
 
-def test_you_can_train_the_eld_on_the_synthetic_dataset_using_squared_circdist():
-    X, y = synthetic.generate_dataset(seed=1686, n_classes=32)
+def test_you_can_train_the_eld_on_the_synthetic_dataset_using_squared_circdist(dataset):
+    X, y = dataset
     X_train, X_test, y_train, y_test = model_selection.train_test_split(
         X, y, train_size=0.8
     )
     model = gdec.EmpiricalLinearDecoder()
     model.fit(X_train, y_train, criterion="squared_circdist")
     score = model.score(X_test, y_test)
-    assert score > 1 / 32  # Better than random guessing?
+    assert score > 1 / 8  # Better than random guessing?
 
 
-def test_you_can_train_the_eld_on_the_synthetic_dataset_using_cross_entropy():
-    X, y = synthetic.generate_dataset(seed=1686, n_classes=32)
+def test_you_can_train_the_eld_on_the_synthetic_dataset_using_cross_entropy(dataset):
+    X, y = dataset
     X_train, X_test, y_train, y_test = model_selection.train_test_split(
         X, y, train_size=0.8
     )
     model = gdec.EmpiricalLinearDecoder()
     model.fit(X_train, y_train, criterion="cross_entropy")
     score = model.score(X_test, y_test)
-    assert score > 1 / 32  # Better than random guessing?
+    assert score > 1 / 8  # Better than random guessing?
--- tests/test_pid.py
+++ tests/test_pid.py
@@ -14,8 +14,8 @@ def dataset() -> Tuple[np.ndarray, np.ndarray]:
     return synthetic.generate_dataset(seed=1634, n_classes=32)
 
 
-def test_you_can_train_pid_on_the_synthetic_dataset():
-    X, y = synthetic.generate_dataset(seed=1634, n_classes=32)
+def test_you_can_train_pid_on_the_synthetic_dataset(dataset):
+    X, y = dataset
     X_train, X_test, y_train, y_test = model_selection.train_test_split(
         X, y, train_size=0.8
     )
