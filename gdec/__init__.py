123adf11263c2b5cd619791602f2a703f1331787
Daniel Greenidge <dev@danielgreenidge.com>
2020-04-13 16:05:57 -0400

    Enable 64 bit mode in JAX

--- gdec/__init__.py
+++ gdec/__init__.py
@@ -1,10 +1,13 @@
 """Linear decoders for angled grating stimuli."""
+from jax.config import config
 from gdec.eld import EmpiricalLinearDecoder
 from gdec.gpgid import GPGaussianIndependentDecoder
 from gdec.gppid import GPPoissonIndependentDecoder
 from gdec.pid import PoissonIndependentDecoder
 from gdec.snd import SuperNeuronDecoder
 from gdec.vgpmd import VariationalGaussianProcessMulticlassDecoder
+
+config.update("jax_enable_x64", True)
 
 __all__ = [
     "EmpiricalLinearDecoder",
