cbbd4dcbe2ccb8803d4c1dec52533183f54f3ff3
Daniel Greenidge <dev@danielgreenidge.com>
2021-02-05 15:09:21 -0500

    Add CI testing workflow

--- setup.py
+++ setup.py
@@ -8,5 +8,21 @@ setup(
     author_email="dev@danielgreenidge.com",
     description="Linear decoders for angled grating stimuli",
     packages=["gdec"],
-    install_requires=["jax", "jaxlib", "numpy", "scikit-learn", "scipy"],
+    install_requires=["numpy", "scikit-learn", "scipy", "tqdm"],
+    extras_require={
+        "dev": [
+            "black",
+            "flake8",
+            "flake8-bugbear",
+            "flake8-blind-except",
+            "flake8-builtins",
+            "flake8-docstrings",
+            "flake8-mutable",
+            "hypothesis",
+            "isort",
+            "mypy",
+            "pep8-naming",
+            "pytest",
+        ]
+    },
 )
