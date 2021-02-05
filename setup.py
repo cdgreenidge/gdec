18eb6d13563674c1293ede8df5501b3e80b4f02b
Daniel Greenidge <dev@danielgreenidge.com>
2021-02-05 16:05:24 -0500

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="gdec",
-    version="0.0.1",
+    version="0.0.2",
     author="C. Daniel Greenidge",
     author_email="dev@danielgreenidge.com",
     description="Linear decoders for angled grating stimuli",
